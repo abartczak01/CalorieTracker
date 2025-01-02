0f13b34b311bab685d867e5247fcf633e480c30f
Agata <agata.bartczak01@gmail.com>
2025-01-02 21:39:14 +0100

    create database connection

---
+++ backend/src/test/java/dev/abartczak/calorietracker/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abartczak.calorietracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
