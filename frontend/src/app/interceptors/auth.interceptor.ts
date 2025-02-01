bbfe255d577972541bdc3d80cb0a606fffc36225
Agata <agata.bartczak01@gmail.com>
2025-02-01 21:39:14 +0100

    frontend: implement user login and registration

---
+++ frontend/src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,8 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router);
+  return next(req);
+};
